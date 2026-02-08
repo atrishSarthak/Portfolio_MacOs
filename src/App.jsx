3b9c37ccd91d4472c6bfadafaf306fba01bdadc5
SARTHAK ATRISH <sarthakatrish@SARTHAKs-MacBook-Air-2.local>
2026-02-09 02:36:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '#components/Navbar'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <Navbar />
+      </div>
+    </main>
+  )
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '#components': resolve(dirname(fileURLToPath(import.meta.url)), "src/components"),
+      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), "src/hoc"),
+      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), "src/constants"),
+      '#windows': resolve(dirname(fileURLToPath(import.meta.url)), "src/windows"),
+    },
+  },
+})
